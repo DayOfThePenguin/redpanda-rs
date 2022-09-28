71ca59f2b27cc980711ab31ba08fa67b5ee531a9
DayOfThePenguin <colin.dablain@gmail.com>
2022-09-28 17:28:56 +0900

    feat(api) add Record struct to public API

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,20 @@ mod tests;
 
 pub use admin::RedPandaAdminClient;
 pub use builder::RedPandaBuilder;
+use chrono::Utc;
 pub use consumer::RedPandaConsumer;
 pub use producer::RedPandaProducer;
 
 pub use rdkafka::error;
 pub use rdkafka::groups;
 pub use rdkafka::message;
+use rdkafka::message::OwnedHeaders;
 pub use rdkafka::statistics;
 pub use rdkafka::types;
+
+pub struct RedPandaRecord {
+    key: Option<Vec<u8>>,
+    value: Option<Vec<u8>>,
+    headers: Option<OwnedHeaders>,
+    timestamp: Option<Utc>,
+}
